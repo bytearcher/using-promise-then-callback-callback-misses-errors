0e69683f1b29f20bd94f2afcdad3c69f5f58010f
pspi <pspi@users.noreply.github.com>
2015-10-29 13:10:52 +0200

    fine tuning

--- test/promise-then-callback-callback.js
+++ test/promise-then-callback-callback.js
@@ -1,17 +1,3 @@
-// var deferredRead = require('../naive-deferredRead');
-
-// var assert = require('assert');
-// var fs = require('fs');
-
-// describe('deferredRead', () => {
-//     it('reads words file correctly', () => {
-//         return deferredRead(fs, "words.txt").then((words) => {
-//             assert.equal(words, "word\n");
-//         });
-//     });
-// });
-
-
 var promise = Promise.resolve();
 
 function throwError() {
